df45b0e34c27f7fc9bdc8c0519f22f928d9cbaa6
mato <tomoya.matou@gmail.com>
2018-12-30 16:48:45 +0900

    refactor dummy data for datetime

--- database/factories/AlbumFactory.php
+++ database/factories/AlbumFactory.php
@@ -5,8 +5,8 @@ use Faker\Generator as Faker;
 $factory->define(App\Album::class, function (Faker $faker) {
     return [
         'spot_id' => rand(1, 50),
-        'started_at' => $faker->dateTimeBetween(),
-        'ended_at' => $faker->dateTimeBetween(),
+        'started_at' => $faker->dateTimeBetween()->format('Y-m-d'),
+        'ended_at' => $faker->dateTimeBetween()->format('Y-m-d'),
         'title' => $faker->realText(20),
         'memo' => $faker->realText(80),
     ];
